ec22969d7e6b1fe162075168d0782110571673e7
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-03-08 14:14:05 -0600

    added inital file and starter code

---
+++ main.py
@@ -0,0 +1,11 @@
+# This project is for generating a flashcards to review
+# I also want to make it only using the python docs
+# It generates a txt file and with the flashcards having a format of
+# --------------------------------------------------------
+# module name: sys
+# description
+# usage
+# my example
+# --------------------------------------------------------
+# start:
+    # create a file with the format .txt
