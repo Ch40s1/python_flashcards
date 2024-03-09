b052f8bf4d006dddb39b4987929815af5e622e5b
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-03-08 22:15:43 -0600

    file gets generated with simple text

--- main.py
+++ main.py
@@ -9,3 +9,16 @@
 # --------------------------------------------------------
 # start:
     # create a file with the format .txt
+def main():
+    file_name = "flashcard.txt"
+
+    #with open creates a new text file. if the file exists then it updates it
+    # open the file in write mode
+    with open(file_name, 'w') as file:
+        # write contetnt to the file
+        file.write("Hello world ?")
+    #print that the file is created
+    print(f"file {file_name} created")
+
+# call the main function
+main()
